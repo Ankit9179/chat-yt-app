18e4f4206fd039e1c6938f3df709b0468ca678e7
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2024-05-16 00:13:12 +0530

    complete sign in & up design

---
+++ frontend/src/pages/signUp/SignUp.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import GenderCheckBox from './GenderCheckBox'
+
+const SignUp = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                <h1 className='text-3xl font-semibold text-center text-white'>SignUp
+                    <span className='text-blue-500'> ChatMet</span>
+                </h1>
+                <form>
+                    <div className='my-2'>
+                        <label className='lable p-2'>
+                            <span className='text-base lable-text text-1xl font-semibold'>Full Name</span>
+                        </label>
+                        <input type="text" className='w-full h-10 px-2 bg-gray-400 text-white rounded-xl' />
+                    </div>
+                    <div className='my-2'>
+                        <label className='lable p-2'>
+                            <span className='text-base lable-text text-1xl font-semibold'>User Name</span>
+                        </label>
+                        <input type="text" className='w-full h-10 px-2 bg-gray-400 text-white rounded-xl' />
+                    </div>
+                    <div className='my-2'>
+                        <label className='lable p-2'>
+                            <span className='text-base lable-text text-1xl font-semibold'>Password</span>
+                        </label>
+                        <input type="password" className='w-full h-10 px-2 bg-gray-400 text-white rounded-xl' />
+                    </div>
+                    <GenderCheckBox />
+                    <div className='my-2'>
+                        <label className='lable p-2'>
+                            <span className='text-base lable-text text-1xl font-semibold'>Confirm Password</span>
+                        </label>
+                        <input type="password" className='w-full h-10 px-2 bg-gray-400 text-white rounded-xl' />
+                    </div>
+                    <a href="#" className='hover:text-blue-400 pt-3 text-1xl'>Already have an account ?</a>
+                    <div className='button my-2'>
+                        <button type="button" className='w-full h-10 px-2 bg-gray-950 text-white rounded-xl hover:bg-white hover:text-black'>Sign Up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
