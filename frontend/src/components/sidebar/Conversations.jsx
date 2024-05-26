805f8abca7d6942c3af8a8951a5c9f332f56c4e0
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2024-05-26 22:41:22 +0530

    come changes in css for phone view

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -6,7 +6,7 @@ const Conversations = () => {
     const { loading, conversations } = useGetConversation();
     console.log("conversations", conversations)
     return (
-        <div className=' py-2 flex flex-col overflow-auto'>
+        <div className=' py-2 flex flex-col h-full sm:overflow-auto'>
             {
                 conversations.map((conver) => (
                     <Conversation
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,28 @@
+// import React from "react";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import MessageContainer from "../../components/messages/MessageContainer";
+
+// const Home = () => {
+//   return <div className=" flex sm:h-[450px] md:h-[550px] bg-gray-400  rounded-lg overflow-hidden bg-clip-padding backdrop:filter backdrop-blur-lg bg-opacity-0">
+//     <Sidebar />
+//     <MessageContainer />
+//   </div>;
+// };
+
+// export default Home;
+
+
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import MessageContainer from "../../components/messages/MessageContainer";
 
 const Home = () => {
-  return <div className=" flex sm:h-[450px] md:h-[550px] bg-gray-400  rounded-lg overflow-hidden bg-clip-padding backdrop:filter backdrop-blur-lg bg-opacity-0">
-    <Sidebar />
-    <MessageContainer />
-  </div>;
+  return (
+    <div className="flex flex-col sm:flex-row h-full overflow-auto sm:h-[450px] md:h-[550px] bg-gray-400 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
 };
 
 export default Home;
